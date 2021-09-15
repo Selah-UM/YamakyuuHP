891ad22098cde4af578b3584e82fee7f039da8b4
Selah <konomi_20n1101220@nnn.ed.jp>
2021-09-15 15:47:08 +0900

    ライブラリ引用の整理

--- assets/javascript/drop-down-menu.js
+++ assets/javascript/drop-down-menu.js
@@ -1,18 +1,18 @@
 $(function() {
   $(".headNav > li").mouseover(function(){
-    // $.ajax({
-      // success: function(){
+    $.ajax({
+      success: function(){
         // $(".headerInner").css('align-items','');
-        $(this).find(".subMenu").show();
-      // }
-    // });
+        $(".headNav > li").find(".subMenu").show();
+      }
+    });
   });
   $(".headNav > li").mouseout(function(){
-    // $.ajax({
-      // success: function(){
+    $.ajax({
+      success: function(){
         // $(".headerInner").css('align-items','center');
-        $(this).find(".subMenu").hide();
-      // }
-    // });
+        $(".headNav > li").find(".subMenu").hide();
+      }
+    });
   });
 });
