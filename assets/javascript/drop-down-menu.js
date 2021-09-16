ea33b4821afb5375ece70db5c1c621eb529e763c
Selah <konomi_20n1101220@nnn.ed.jp>
2021-09-16 15:41:15 +0900

    drop down menu 改良

--- assets/javascript/drop-down-menu.js
+++ assets/javascript/drop-down-menu.js
@@ -1,23 +1,35 @@
 $(function() {
+  $(".headerInner").css('align-items','flex-end');
   $(".headNav > li").mouseover(function(){
-    $.ajax({
-      success: function(){
-        // $(".headerInner").css('align-items','');
-        $(".headNav > li").find(".subMenu").show();
-      }
-    });
+    // $.ajax({
+    //   success: function(){
+        new Promise((resolve) => {
+          const item = this;
+          $(".headerInner").css('align-items','');
+          resolve(item);
+        }).then((item) => {
+          $(item).find(".subMenu").show();
+        });
+      // });
   });
   $(".headNav > li").mouseout(function(){
-    $.ajax({
-      success: function(){
-        // $(".headerInner").css('align-items','center');
-        $(".headNav > li").find(".subMenu").hide();
-      }
-    });
+    // $.ajax({
+    //   success: function(){
+      new Promise((resolve) => {
+        const item = this;
+        $(".headerInner").css('align-items','flex-end');
+        resolve(item);
+      }).then((item) => {
+        $(item).find(".subMenu").hide();
+      });
+    //   }
+    // });
   });
 });
 
 /** 
+ * css 変更を反映させるためにはコマンドシフトR
+ * 
  * my-website　で　docker　起動しコンソールに入る
  * my-fist に移って python3 -m http.server 8000
  * http://localhost:8000/
