2e45a02725768a6d580b21408ef66989d845c928
Selah <konomi_20n1101220@nnn.ed.jp>
2021-09-28 14:29:37 +0900

    background調整

--- assets/javascript/drop-down-menu.js
+++ assets/javascript/drop-down-menu.js
@@ -1,29 +1,25 @@
 $(function() {
   $(".headerInner").css('align-items','flex-end');
-  $(".headNav > li").mouseover(function(){
-    // $.ajax({
-    //   success: function(){
-        new Promise((resolve) => {
-          const item = this;
-          $(".headerInner").css('align-items','');
-          resolve(item);
-        }).then((item) => {
-          $(item).find(".subMenu").show();
-        });
-      // });
+      // $("header").css('background-color','#348da9');
+      $(".headNav > li").mouseover(function(){
+    new Promise((resolve) => {
+      const item = this;
+      $(".headerInner").css('align-items','');
+      // $("header").css('background-color','');
+      resolve(item);
+    }).then((item) => {
+      $(item).find(".subMenu").show();
+    });
   });
   $(".headNav > li").mouseout(function(){
-    // $.ajax({
-    //   success: function(){
-      new Promise((resolve) => {
-        const item = this;
-        $(".headerInner").css('align-items','flex-end');
-        resolve(item);
-      }).then((item) => {
-        $(item).find(".subMenu").hide();
-      });
-    //   }
-    // });
+    new Promise((resolve) => {
+      const item = this;
+      $(".headerInner").css('align-items','flex-end');
+      // $("header").css('background-color','#348da9');
+      resolve(item);
+    }).then((item) => {
+      $(item).find(".subMenu").hide();
+    });
   });
 });
 
